42c8e169888f7acd2328a5aceb32d14085686c03
Emmanuel <emmanuelappiah124@hotmail.com>
2023-10-20 05:46:15 +0100

    React UI assemblies

---
+++ tailwindreact/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Homepage from "./pages/HomePage";
+
+export default function App() {
+  return (
+    <div>
+      <Homepage></Homepage>
+    </div>
+  );
+}
---
+++ tailwindreact/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Sections/Hero";
+import Feature from "../components/Sections/Feature";
+import Cta from "../components/Sections/CTA";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Hero></Hero>
+      <Feature></Feature>
+      <Cta></Cta>
+    </div>
+  );
+}
