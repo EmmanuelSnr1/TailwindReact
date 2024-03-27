e44e3356cba0fcf824ff9684e8d536835ae6eeee
Emmanuel <emmanuelappiah124@hotmail.com>
2024-03-27 06:44:42 +0000

    Checked this project out for a bit.

--- tailwindreact/src/pages/HomePage.jsx
+++ tailwindreact/src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import Hero from "../components/Sections/Hero";
 import Feature from "../components/Sections/Feature";
 import Cta from "../components/Sections/CTA";
+import Newsletter from "../components/Sections/Newsletter";
+import Blog from "../components/Sections/Blog";
 
 export default function Homepage() {
   return (
@@ -8,6 +10,7 @@ export default function Homepage() {
       <Hero></Hero>
       <Feature></Feature>
       <Cta></Cta>
+      <Newsletter></Newsletter>
     </div>
   );
 }
